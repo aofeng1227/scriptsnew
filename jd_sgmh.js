d8c6e43b67bb112c9f2fd02adf27e2fa9f97f2d3
aofeng1227 <liujiang19861227@163.com>
2021-11-10 15:54:40 +0800

    更新

--- jd_sgmh.js
+++ jd_sgmh.js
@@ -24,7 +24,7 @@ const $ = new Env('闪购盲盒');
 const jdCookieNode = $.isNode() ? require('./utils/jdCookie.js') : '';
 let appId = '1EFRXxg' , homeDataFunPrefix = 'interact_template', collectScoreFunPrefix = 'harmony', message = ''
 let lotteryResultFunPrefix = homeDataFunPrefix, browseTime = 6
-const inviteCodes = [];
+const inviteCodes = ['@','@'];
 const randomCount = $.isNode() ? 20 : 5;
 const notify = $.isNode() ? require('./sendNotify') : '';
 let merge = {}
