87d41c14f9dc9cebbd9950f1d998a1c3375a972b
aofeng1227 <liujiang19861227@163.com>
2021-11-15 00:52:57 +0800

    更新

---
+++ sh/config_sample.sh
@@ -0,0 +1,177 @@
+## Version: v2.8.0
+## Date: 2021-06-20
+## Update Content: 可持续发展纲要\n1. session管理破坏性修改\n2. 配置管理可编辑config下文件\n3. 自定义脚本改为查看脚本\n4. 移除互助相关
+
+## 上面版本号中，如果第2位数字有变化，那么代表增加了新的参数，如果只有第3位数字有变化，仅代表更新了注释，没有增加新的参数，可更新可不更新
+
+## 在运行 ql repo 命令时，是否自动删除失效的脚本与定时任务
+AutoDelCron="true"
+
+## 在运行 ql repo 命令时，是否自动增加新的本地定时任务
+AutoAddCron="true"
+
+## 拉取脚本时默认的定时规则，当匹配不到定时规则时使用，例如: 0 9 * * *
+DefaultCronRule=""
+
+## ql repo命令拉取脚本时需要拉取的文件后缀，直接写文件后缀名即可
+RepoFileExtensions="js py"
+
+## 由于github仓库拉取较慢，所以会默认添加代理前缀，如不需要请移除
+GithubProxyUrl="https://ghproxy.com/"
+
+## 设置定时任务执行的超时时间，默认1h，后缀"s"代表秒(默认值), "m"代表分, "h"代表小时, "d"代表天
+CommandTimeoutTime="1h"
+
+## 设置批量执行任务时的并发数，默认同时执行5个任务
+MaxConcurrentNum="40"
+
+## 在运行 task 命令时，随机延迟启动任务的最大延迟时间
+## 默认给javascript任务加随机延迟，如 RandomDelay="300" ，表示任务将在 1-300 秒内随机延迟一个秒数，然后再运行，取消延迟赋值为空
+RandomDelay="300"
+
+## 如果你自己会写shell脚本，并且希望在每次运行 ql update 命令时，额外运行你的 shell 脚本，请赋值为 "true"，默认为true
+EnableExtraShell="true"
+
+## 是否自动启动bot，默认不启动，设置为true时自动启动，目前需要自行克隆bot仓库所需代码，存到ql/repo目录下，文件夹命名为dockerbot
+AutoStartBot=""
+
+## 安装bot依赖时指定pip源，默认使用清华源，如不需要源，设置此参数为空
+PipMirror="https://pypi.tuna.tsinghua.edu.cn/simple"
+
+## 通知环境变量
+## 1. Server酱
+## https://sct.ftqq.com
+## 下方填写 SCHKEY 值或 SendKey 值
+export PUSH_KEY=""
+
+## 2. BARK
+## 下方填写app提供的设备码，例如：https://api.day.app/123 那么此处的设备码就是123
+export BARK_PUSH=""
+## 下方填写推送声音设置，例如choo，具体值请在bark-推送铃声-查看所有铃声
+export BARK_SOUND=""
+## 下方填写推送消息分组，默认为"QingLong"
+export BARK_GROUP="QingLong"
+
+## 3. Telegram 
+## 下方填写自己申请@BotFather的Token，如10xxx4:AAFcqxxxxgER5uw
+export TG_BOT_TOKEN=""
+## 下方填写 @getuseridbot 中获取到的纯数字ID
+export TG_USER_ID=""
+## Telegram 代理IP（选填）
+## 下方填写代理IP地址，代理类型为 http，比如您代理是 http://127.0.0.1:1080，则填写 "127.0.0.1"
+## 如需使用，请自行解除下一行的注释
+export TG_PROXY_HOST=""
+## Telegram 代理端口（选填）
+## 下方填写代理端口号，代理类型为 http，比如您代理是 http://127.0.0.1:1080，则填写 "1080"
+## 如需使用，请自行解除下一行的注释
+export TG_PROXY_PORT=""
+## Telegram 代理的认证参数（选填）
+export TG_PROXY_AUTH=""
+## Telegram api自建反向代理地址（选填）
+## 教程：https://www.hostloc.com/thread-805441-1-1.html
+## 如反向代理地址 http://aaa.bbb.ccc 则填写 aaa.bbb.ccc
+## 如需使用，请赋值代理地址链接，并自行解除下一行的注释
+export TG_API_HOST=""
+
+## 4. 钉钉 
+## 官方文档：https://developers.dingtalk.com/document/app/custom-robot-access
+## 下方填写token后面的内容，只需 https://oapi.dingtalk.com/robot/send?access_token=XXX 等于=符号后面的XXX即可
+export DD_BOT_TOKEN=""
+export DD_BOT_SECRET=""
+
+## 5. 企业微信机器人
+## 官方说明文档：https://work.weixin.qq.com/api/doc/90000/90136/91770
+## 下方填写密钥，企业微信推送 webhook 后面的 key
+export QYWX_KEY=""
+
+## 6. 企业微信应用
+## 参考文档：http://note.youdao.com/s/HMiudGkb
+## 下方填写素材库图片id（corpid,corpsecret,touser,agentid），素材库图片填0为图文消息, 填1为纯文本消息
+export QYWX_AM="ww03a509cae592fa88,EPh-QXC9miV3f2UVIcuh_zaVIUCSY5EFJB8fGWtLEqo,@all,1000002"
+
+## 7. iGot聚合
+## 参考文档：https://wahao.github.io/Bark-MP-helper
+## 下方填写iGot的推送key，支持多方式推送，确保消息可达
+export IGOT_PUSH_KEY=""
+
+## 8. Push Plus
+## 官方网站：http://www.pushplus.plus
+## 下方填写您的Token，微信扫码登录后一对一推送或一对多推送下面的token，只填 PUSH_PLUS_TOKEN 默认为一对一推送
+export PUSH_PLUS_TOKEN=""
+## 一对一多推送（选填）
+## 下方填写您的一对多推送的 "群组编码" ，（一对多推送下面->您的群组(如无则新建)->群组编码）
+## 1. 需订阅者扫描二维码 2、如果您是创建群组所属人，也需点击“查看二维码”扫描绑定，否则不能接受群组消息推送
+export PUSH_PLUS_USER=""
+
+## 9. go-cqhttp
+## gobot_url 推送到个人QQ: http://127.0.0.1/send_private_msg  群：http://127.0.0.1/send_group_msg 
+## gobot_token 填写在go-cqhttp文件设置的访问密钥
+## gobot_qq 如果GOBOT_URL设置 /send_private_msg 则需要填入 user_id=个人QQ 相反如果是 /send_group_msg 则需要填入 group_id=QQ群 
+## go-cqhttp相关API https://docs.go-cqhttp.org/api
+export GOBOT_URL=""
+export GOBOT_TOKEN=""
+export GOBOT_QQ=""
+
+## 其他需要的变量，脚本中需要的变量使用 export 变量名= 声明即可
+
+
+## 双十一无门槛红包
+export FLCODE="yKy4B8e"
+
+## 愤怒的锦鲤
+### 在kois环境变量中填入需要助力的pt_pin，有多个请用@符号连接
+export kois="jd_45af13f824f4a@jd_76d60b83821e3@jd_lltyhbgFeCTQ"
+
+## 京东多合一签到
+### 推送通知默认简洁模式(多账号只发送一次)。如需详细通知，设置环境变量 JD_BEAN_SIGN_NOTIFY_SIMPLE 为false即可(N账号推送N次通知)。
+export JD_BEAN_SIGN_NOTIFY_SIMPLE="false"
+
+## 清空购物车参数
+export JD_CART_REMOVE="false" ## 是否清空，如果为false，则上面设置了多少就只删除多少条
+export JD_CART_REMOVESIZE="20" ## 运行一次取消多全部已关注的商品。数字0表示不取关任何商品
+export JD_CART_REMOVEALL="false" ## 是否清空，如果为false，则上面设置了多少就只删除多少条
+
+## 东东世界 兑换京豆变量
+### 默认999,京豆从多到少兑换，先兑换1000，1000兑换完后兑换500
+### "6"只兑换1000京豆, "5"只兑换500京豆, "1"只兑换200京豆
+export DDEXCHANGE="6" 
+
+## 京喜工厂通知级别
+### 通知级别 1=生产完毕可兑换通知;2=可兑换通知+生产超时通知+兑换超时通知;3=可兑换通知+生产超时通知+兑换超时通知+未选择商品生产通知(前提：已开通京喜工厂活动);默认第2种通知
+export JXGC_NOTIFY_LEVEL="3"
+
+## 发财大赢家助力
+### 邀请码变量，不支持多账号，格式：redEnvelopeId@markedPin(猜测pt_pin)
+export dyjCode="9f1722f0077e455794e9164109f6cd6656131636563896821@jd_45af13f824f4a"
+
+## 汪汪乐园每日任务
+### 脚本默认会帮我助力开工位，介意请添加变量HELP_JOYPARK，false为不助力
+export HELP_JOYPARK="false"
+
+##  柠檬旺财乐园新手上路版
+### 自定义邀请码变量 IANWqUmbgQVF9ePHGsGFA2m-zSTLKmHFbE-IW-Waarw
+export joyinviterPin="wSbECU1ILnIMa3CrHBAC1pioljIiQgcY_mvvi9TKHss" 
+
+## 惊喜牧场
+### 环境变量：JX_USER_AGENT, 惊喜APP的UA。领取助力任务奖励需要惊喜APP的UA,有能力的可以填上自己的UA,默认生成随机UA
+### 环境变量：BYTYPE,购买小鸡品种，默认不购买,(ps:暂时不知道买哪个好)
+### BYTYPE="1",购买小黄鸡，BYTYPE="2",购买辣子鸡，BYTYPE="3",购买椰子鸡,BYTYPE="4",购买猪肚鸡,BYTYPE="999",能买哪只买哪只,BYTYPE="888",不购买小鸡
+### 脚本9点-10点才会执行内部助力
+export BYTYPE="999"
+
+## 早起赢现金
+### 京东汽车-瓜分万元
+### 支付一元才能参与活动，填写环境变量morningScPins给指定账号打卡
+export morningScPins="jd_45af13f824f4a"
+
+## 京东试用
+export JD_TRY="true"
+
+## 柠檬推一推
+### 极速版赚金币 分享到QQ查看邀请码 packetId就是
+export tytpacketId="f633a7deede94ca79ac81488d3f11414-amRfNDVhZjEzZjgyNGY0YQ!!"
+
+## 入口：我的-赚30
+### 备注：赚30元每日签到红包、天降红包助力，在earn30Pins环境变量中填入需要签到和接受助力的账号
+### 技巧：每月可以提现100元，但需要邀请一个新人下首单。可以用已注册手机号重新注册为新人账号，切换ip可以提高成功率。
+export earn30Pins="jd_45af13f824f4a"
