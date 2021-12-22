62d62cd2d69d8dab2e051c4816cd856bd2400237
aofeng1227 <liujiang19861227@163.com>
2021-12-22 21:25:42 +0800

    更新

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -76,6 +76,26 @@ let TempBaipiao = "";
 if ($.isNode() && process.env.WP_APP_TOKEN_ONE) {
 	WP_APP_TOKEN_ONE = process.env.WP_APP_TOKEN_ONE;
 }
+
+let doExJxBeans ="false";
+let time = new Date().getHours();
+if ($.isNode()) {
+	if(process.env.BEANCHANGE_ExJxBeans=="true"){
+		if (time >= 17){ 
+			console.log(`检测到设定了临期京豆转换喜豆...`);
+			doExJxBeans = process.env.BEANCHANGE_ExJxBeans;
+		} else{
+			console.log(`检测到设定了临期京豆转换喜豆,但时间未到17点后，暂不执行转换...`);
+		}
+	}
+}
+let doCheckJxBeans ="false";
+if ($.isNode()) {
+	if(process.env.BEANCHANGE_CheckJxBeans=="true"){
+		console.log(`检测到设定了查询喜豆信息...`);
+		doCheckJxBeans = process.env.BEANCHANGE_ExJxBeans;	
+	}
+}
 if ($.isNode() && process.env.BEANCHANGE_PERSENT) {
 	intPerSent = parseInt(process.env.BEANCHANGE_PERSENT);
 	console.log(`检测到设定了分段通知:` + intPerSent);
@@ -102,10 +122,13 @@ if ($.isNode() && process.env.BEANCHANGE_USERGP4) {
 if ($.isNode() && process.env.BEANCHANGE_DISABLECASH) {
 	DisableCash = process.env.BEANCHANGE_DISABLECASH;
 }
+
+
 //取消月结查询
 //if ($.isNode() && process.env.BEANCHANGE_ENABLEMONTH) {
 	//EnableMonth = process.env.BEANCHANGE_ENABLEMONTH;
 //}
+
 if ($.isNode() && process.env.BEANCHANGE_ALLNOTIFY) {
 	
 	var strTempNotify=process.env.BEANCHANGE_ALLNOTIFY ? process.env.BEANCHANGE_ALLNOTIFY.split('&') : [];
@@ -126,6 +149,9 @@ let userIndex2 = -1;
 let userIndex3 = -1;
 let userIndex4 = -1;
 
+
+let decExBean=0;
+
 if ($.isNode()) {
 	Object.keys(jdCookieNode).forEach((item) => {
 		cookiesArr.push(jdCookieNode[item])
@@ -135,6 +161,8 @@ if ($.isNode()) {
 } else {
 	cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
+
+
 !(async() => {
 	if (!cookiesArr[0]) {
 		$.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {
@@ -181,7 +209,13 @@ if ($.isNode()) {
 			$.JingXiang = "";
 			$.allincomeBean = 0; //月收入
 			$.allexpenseBean = 0; //月支出
-			$.joylevel = 0;
+			$.joylevel = 0;	
+			$.beanChangeXi=0;
+			$.inJxBean=0;
+			$.OutJxBean=0;
+			$.todayinJxBean=0;
+			$.todayOutJxBean=0;	
+			$.xibeanCount = 0;
 			TempBaipiao = "";
 			strGuoqi="";
 			console.log(`******开始查询【京东账号${$.index}】${$.nickName || $.UserName}*********`);
@@ -215,7 +249,7 @@ if ($.isNode()) {
 			await requestAlgo();
 			await JxmcGetRequest();
 			await bean();
-
+			
 			if (llShowMonth) {
 				console.log("开始获取月数据，请稍后...");
 				await Monthbean();
@@ -228,7 +262,10 @@ if ($.isNode()) {
 			if (DisableCash == "false") {
 				await jdCash();
 			}
-
+			if(doCheckJxBeans=="true"){
+				await GetJxBeanInfo();
+				await jxbean();
+			}
 			await showMsg();
 			if (intPerSent > 0) {
 				if ((i + 1) % intPerSent == 0) {
@@ -418,7 +455,7 @@ async function showMsg() {
 	//return
 	ReturnMessageTitle="";
 	ReturnMessage = "";
-	
+	var strsummary="";
 	if (MessageUserGp2) {
 		userIndex2 = MessageUserGp2.findIndex((item) => item === $.pt_pin);
 	}
@@ -507,28 +544,58 @@ async function showMsg() {
 	}
 
 	ReturnMessage += `【今日京豆】收${$.todayIncomeBean}豆`;
-
+	strsummary+= `【今日京豆】收${$.todayIncomeBean}豆`;
 	if ($.todayOutcomeBean != 0) {
 		ReturnMessage += `,支${$.todayOutcomeBean}豆`;
+		strsummary += `,支${$.todayOutcomeBean}豆`;
 	}
 	ReturnMessage += `\n`;
-
+	strsummary+= `\n`;
 	ReturnMessage += `【昨日京豆】收${$.incomeBean}豆`;
-
+	strsummary+= `【昨日京豆】收${$.incomeBean}豆`;
 	if ($.expenseBean != 0) {
 		ReturnMessage += `,支${$.expenseBean}豆`;
+		strsummary += `,支${$.expenseBean}豆`;
 	}
 	ReturnMessage += `\n`;	
-	
-	if ($.levelName || $.JingXiang){
-		ReturnMessage += `【当前京豆】${$.beanCount}豆(≈${($.beanCount / 100).toFixed(2)}元)\n`;
+	strsummary += `\n`;	
+	if ($.beanCount){		
+		ReturnMessage += `【当前京豆】${$.beanCount}豆(≈${(($.beanCount-$.beanChangeXi)/ 100).toFixed(2)}元)\n`;
+		strsummary+= `【当前京豆】${$.beanCount}豆(≈${(($.beanCount-$.beanChangeXi)/ 100).toFixed(2)}元)\n`;	
 	} else {
-		ReturnMessage += `【当前京豆】获取失败,接口返回空数据\n`;
+		if($.levelName || $.JingXiang)
+			ReturnMessage += `【当前京豆】获取失败,接口返回空数据\n`;
+		else{
+			ReturnMessage += `【当前京豆】${$.beanCount}豆(≈${(($.beanCount-$.beanChangeXi)/ 100).toFixed(2)}元)\n`;
+			strsummary += `【当前京豆】${$.beanCount}豆(≈${(($.beanCount-$.beanChangeXi)/ 100).toFixed(2)}元)\n`;
+		}			
 	}
+	
+	if (doCheckJxBeans == "true") {
+		ReturnMessage += `【今日喜豆】收${$.todayinJxBean}豆`;
+		strsummary+= `【今日喜豆】收${$.todayinJxBean}豆`;
+		if ($.todayOutJxBean != 0) {
+			ReturnMessage += `,支${$.todayOutJxBean}豆`;
+			strsummary += `,支${$.todayOutJxBean}豆`;
+		}
+		ReturnMessage += `\n`;
+		strsummary += `\n`;
+		ReturnMessage += `【昨日喜豆】收${$.inJxBean}豆`;
+		strsummary += `【昨日喜豆】收${$.inJxBean}豆`;
+		if ($.OutJxBean != 0) {
+			ReturnMessage += `,支${$.OutJxBean}豆`;
+			strsummary += `,支${$.OutJxBean}豆`;
+		}
+		ReturnMessage += `\n`;
+		strsummary += `\n`;
+		ReturnMessage += `【当前喜豆】${$.xibeanCount}喜豆(≈${($.xibeanCount/ 100).toFixed(2)}元)\n`;
+		strsummary += `【当前喜豆】${$.xibeanCount}喜豆(≈${($.xibeanCount/ 100).toFixed(2)}元)\n`;
+	}
+
 
 	if (typeof $.JDEggcnt !== "undefined") {
 		if ($.JDEggcnt == 0) {
-			ReturnMessage += `【京喜牧场】未开通或提示火爆.\n`;
+			//ReturnMessage += `【京喜牧场】未开通或提示火爆.\n`;
 		} else {
 			ReturnMessage += `【京喜牧场】${$.JDEggcnt}枚鸡蛋\n`;
 		}
@@ -744,16 +811,20 @@ async function showMsg() {
 	console.log(`${ReturnMessageTitle+ReturnMessage}`);
 
 	if ($.isNode() && WP_APP_TOKEN_ONE) {
+		var strTitle="京东资产变动";
+		
 		if (TempBaipiao) {
+			strsummary=TempBaipiao + `\n` +strsummary;			
 			TempBaipiao = `【⏰商品白嫖活动提醒⏰】\n` + TempBaipiao;
-			ReturnMessage = TempBaipiao + `\n` + ReturnMessage;
+			ReturnMessage = TempBaipiao + `\n` + ReturnMessage;			
 		}
 		ReturnMessage=`【账号名称】${$.nickName || $.UserName}\n`+ReturnMessage;
+		
 		ReturnMessage += RemainMessage;
 		if(strAllNotify)
 			ReturnMessage=strAllNotify+`\n`+ReturnMessage;
 		
-		await notify.sendNotifybyWxPucher(`${$.name}`, `${ReturnMessage}`, `${$.UserName}`);
+		await notify.sendNotifybyWxPucher(strTitle, `${ReturnMessage}`, `${$.UserName}`,'\n\n本通知 By ccwav Mod',strsummary);
 	}
 
 	//$.msg($.name, '', ReturnMessage , {"open-url": "https://bean.m.jd.com/beanDetail/index.action?resourceValue=bean"});
@@ -822,7 +893,18 @@ async function bean() {
 	}
 	$.todayOutcomeBean = -$.todayOutcomeBean;
 	$.expenseBean = -$.expenseBean;
+	
+	decExBean =0;
 	await queryexpirejingdou();//过期京豆
+	if(decExBean && doExJxBeans=="true"){
+		var jxbeans = await exchangejxbeans(decExBean);
+		if (jxbeans) {			
+			$.beanChangeXi=decExBean;
+			console.log(`已为您将`+decExBean+`临期京豆转换成喜豆！`);
+			strGuoqi += `已为您将`+decExBean+`临期京豆转换成喜豆！\n`;
+		}
+	}
+	
 	await redPacket(); 
 }
 
@@ -1184,12 +1266,14 @@ function queryexpirejingdou() {
 					if (data) {
 						// console.log(data)
 						data = JSON.parse(data.slice(23, -13));
-						if (data.ret === 0) {
+						if (data.ret === 0) {							
 							data['expirejingdou'].map(item => {
 								if(item['expireamount']!=0){																	
 									strGuoqi+=`【${timeFormat(item['time'] * 1000)}】过期${item['expireamount']}豆\n`;
+									if (decExBean==0)
+										decExBean=item['expireamount'];
 								}
-							})
+							})							
 						}
 					} else {
 						console.log(`京东服务器返回空数据`)
@@ -1203,6 +1287,49 @@ function queryexpirejingdou() {
 			}
 		})
 	})
+}
+function exchangejxbeans(o) {
+    return new Promise(async resolve => {
+		var UUID = getUUID('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');		
+		var JXUA = `jdpingou;iPhone;4.13.0;14.4.2;${UUID};network/wifi;model/iPhone10,2;appBuild/100609;ADID/00000000-0000-0000-0000-000000000000;supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/${Math.random * 98 + 1};pap/JA2019_3111789;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148`;
+        const options = {
+            "url": `https://m.jingxi.com/deal/masset/jd2xd?use=${o}&canpintuan=&setdefcoupon=0&r=${Math.random()}&sceneval=2`,
+            "headers": {
+                "Host": "m.jingxi.com",
+                "Accept": "*/*",
+                "Cookie": cookie,
+                "Connection": "keep-alive",
+                "User-Agent": JXUA,
+                "Accept-Language": "zh-cn",
+                "Referer": "https://m.jingxi.com/deal/confirmorder/main",
+                "Accept-Encoding": "gzip, deflate, br",
+            }
+        }
+        $.get(options, (err, resp, data) => {
+            try {
+                if (err) {
+                    console.log(err);
+                } else {
+                    data = JSON.parse(data);
+                    if (data && data.data && JSON.stringify(data.data) === '{}') {
+                        console.log(JSON.stringify(data))
+                    }
+                }
+            } catch (e) {
+                $.logErr(e, resp)
+            } finally {
+                resolve(data || {});
+            }
+        })
+    })
+}
+function getUUID(x = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", t = 0) {
+    return x.replace(/[xy]/g, function (x) {
+        var r = 16 * Math.random() | 0,
+        n = "x" == x ? r : 3 & r | 8;
+        return uuid = t ? n.toString(36).toUpperCase() : n.toString(36),
+        uuid
+    })
 }
 
 function redPacket() {
@@ -1867,6 +1994,126 @@ function taskPostClientActionUrl(body) {
 		}
 	}
 }
+
+function taskJxUrl(functionId, body = '') {
+    let url = ``;
+    var UA = `jdpingou;iPhone;4.13.0;14.4.2;${randomString(40)};network/wifi;model/iPhone10,2;appBuild/100609;supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/${Math.random * 98 + 1};pap/JA2019_3111789;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148`;
+
+    if (body) {
+        url = `https://m.jingxi.com/activeapi/${functionId}?${body}`;
+        url += `&_=${Date.now() + 2}&sceneval=2&g_login_type=1&callback=jsonpCBK${String.fromCharCode(Math.floor(Math.random() * 26) + "A".charCodeAt(0))}&g_ty=ls`;
+    } else {
+        url = `https://m.jingxi.com/activeapi/${functionId}?_=${Date.now() + 2}&sceneval=2&g_login_type=1&callback=jsonpCBK${String.fromCharCode(Math.floor(Math.random() * 26) + "A".charCodeAt(0))}&g_ty=ls`;
+    }
+    return {
+        url,
+        headers: {
+            "Host": "m.jingxi.com",
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate, br",
+            "User-Agent": UA,
+            "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+            "Referer": "https://st.jingxi.com/",
+            "Cookie": cookie
+        }
+    }
+}
+
+
+function GetJxBeanDetailData() {
+  return new Promise((resolve) => {
+    $.get(taskJxUrl("queryuserjingdoudetail","pagesize=10&type=16"), async (err, resp, data) => {
+        try {
+          if (err) {
+            console.log(JSON.stringify(err));
+            console.log(`GetJxBeanDetailData请求失败，请检查网路重试`);
+          } else {
+            data = JSON.parse(data.match(new RegExp(/jsonpCBK.?\((.*);*/))[1]);      
+            
+          }
+        } catch (e) {
+          $.logErr(e, resp);
+        } finally {
+          resolve(data);
+        }
+      });
+  });
+}
+function GetJxBeanInfo() {
+  return new Promise((resolve) => {
+    $.get(taskJxUrl("querybeanamount"), async (err, resp, data) => {
+        try {
+          if (err) {
+            console.log(JSON.stringify(err));
+            console.log(`GetJxBeanInfo请求失败，请检查网路重试`);
+          } else {
+            data = JSON.parse(data.match(new RegExp(/jsonpCBK.?\((.*);*/))[1]);      
+            if(data){
+				if(data.errcode==0){
+					$.xibeanCount=data.data.xibean;
+					if(!$.beanCount){
+						$.beanCount=data.data.jingbean;
+					}
+				}
+			}
+          }
+        } catch (e) {
+          $.logErr(e, resp);
+        } finally {
+          resolve(data);
+        }
+      });
+  });
+}
+async function jxbean() {
+    //前一天的0:0:0时间戳
+    const tm = parseInt((Date.now() + 28800000) / 86400000) * 86400000 - 28800000 - (24 * 60 * 60 * 1000);
+    // 今天0:0:0时间戳
+    const tm1 = parseInt((Date.now() + 28800000) / 86400000) * 86400000 - 28800000;
+    var JxYesterdayArr = [],
+    JxTodayArr = [];
+    var JxResponse = await GetJxBeanDetailData();
+    if (JxResponse && JxResponse.ret == "0") {
+        var Jxdetail = JxResponse.detail;
+        if (Jxdetail && Jxdetail.length > 0) {
+            for (let item of Jxdetail) {
+                const date = item.createdate.replace(/-/g, '/') + "+08:00";
+                if (new Date(date).getTime() >= tm1 && (!item['visibleinfo'].includes("退还") && !item['visibleinfo'].includes('扣赠'))) {
+                    JxTodayArr.push(item);
+                } else if (tm <= new Date(date).getTime() && new Date(date).getTime() < tm1 && (!item['visibleinfo'].includes("退还") && !item['visibleinfo'].includes('扣赠'))) {
+                    //昨日的
+                    JxYesterdayArr.push(item);
+                } else if (tm > new Date(date).getTime()) {
+                    break;
+                }
+            }
+        } else {
+            $.errorMsg = `数据异常`;
+            $.msg($.name, ``, `账号${$.index}：${$.nickName}\n${$.errorMsg}`);
+        }
+
+        for (let item of JxYesterdayArr) {
+            if (Number(item.amount) > 0) {
+                $.inJxBean += Number(item.amount);
+            } else if (Number(item.amount) < 0) {
+                $.OutJxBean += Number(item.amount);
+            }
+        }
+        for (let item of JxTodayArr) {
+            if (Number(item.amount) > 0) {
+                $.todayinJxBean += Number(item.amount);
+            } else if (Number(item.amount) < 0) {
+                $.todayOutJxBean += Number(item.amount);
+            }
+        }
+		$.todayOutJxBean = -$.todayOutJxBean;
+		$.OutJxBean = -$.OutJxBean;
+    }
+
+}
+
+
+	
 function randomString(e) {
 	e = e || 32;
 	let t = "0123456789abcdef",
